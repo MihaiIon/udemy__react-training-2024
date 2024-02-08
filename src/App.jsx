0e8fc8dc8d8387229bae73ec72a36e7390664754
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-02-08 10:52:21 -0500

    added missing header

--- src/App.jsx
+++ src/App.jsx
@@ -19,36 +19,42 @@ function App() {
   } = useGame();
 
   return (
-    <main>
-      <div id="game-container">
-        <ol id="players" className="highlight-player">
-          <Player
-            symbol={player1.symbol}
-            playerName={player1.name}
-            isActive={currentPlayerIndex === 0}
-            onPlayerNameChange={newPlayerName => setPlayer1({ ...player1, name: newPlayerName })}
+    <>
+      <header>
+        <img src="game-logo.png" alt="Tic Tac Toe" />
+        <h1>Tic Tac Toe</h1>
+      </header>
+      <main>
+        <div id="game-container">
+          <ol id="players" className="highlight-player">
+            <Player
+              symbol={player1.symbol}
+              playerName={player1.name}
+              isActive={currentPlayerIndex === 0}
+              onPlayerNameChange={newPlayerName => setPlayer1({ ...player1, name: newPlayerName })}
+            />
+            <Player
+              symbol={player2.symbol}
+              playerName={player2.name}
+              isActive={currentPlayerIndex === 1}
+              onPlayerNameChange={newPlayerName => setPlayer2({ ...player2, name: newPlayerName })}
+            />
+          </ol>
+          {gameState.isGameOver && (
+            <GameOverOverlay
+              winner={gameState.winner}
+              onRestart={handleRestart}
+            />
+          )}
+          <GameBoard
+            gameState={gameState}
+            currentPlayer={currentPlayerIndex === 0 ? player1 : player2}
+            onPlayerAction={handlePlayerAction}
           />
-          <Player
-            symbol={player2.symbol}
-            playerName={player2.name}
-            isActive={currentPlayerIndex === 1}
-            onPlayerNameChange={newPlayerName => setPlayer2({ ...player2, name: newPlayerName })}
-          />
-        </ol>
-        {gameState.isGameOver && (
-          <GameOverOverlay
-            winner={gameState.winner}
-            onRestart={handleRestart}
-          />
-        )}
-        <GameBoard
-          gameState={gameState}
-          currentPlayer={currentPlayerIndex === 0 ? player1 : player2}
-          onPlayerAction={handlePlayerAction}
-        />
-      </div>
-      <Log turnsHistory={turnsHistory} />
-    </main>
+        </div>
+        <Log turnsHistory={turnsHistory} />
+      </main>
+    </>
   )
 }
 
