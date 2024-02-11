68845c78f6a14c21e0b550a8304c8b485962d6b7
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-02-11 11:41:23 -0500

    calculator - fixed calculations

--- investment calculator/src/components/InvestmentsResult.jsx
+++ investment calculator/src/components/InvestmentsResult.jsx
@@ -1,33 +1,31 @@
-import { calculateInvestmentResults, formatter } from '../util/investment';
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function InvestmentsResult({ formValues }) {
   const results = calculateInvestmentResults(formValues);
   console.log(results);
 
   return (
-    <div id="result">
-      <table>
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Investment Amount</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Investment Capital</th>
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Amount</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Investment Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((result, index) => (
+          <tr key={index}>
+            <td>{result.year}</td>
+            <td>{formatter.format(result.valueEndOfYear)}</td>
+            <td>{formatter.format(result.interest)}</td>
+            <td>{formatter.format(result.totalInterest)}</td>
+            <td>{formatter.format(result.totalAmountInvested)}</td>
           </tr>
-        </thead>
-        <tbody>
-          {results.map((result, index) => (
-            <tr key={index}>
-              <td>{result.year}</td>
-              <td>{formatter.format(result.annualInvestment)}</td>
-              <td>{formatter.format(result.interest)}</td>
-              <td>{formatter.format(result.interest)}</td>
-              <td>{formatter.format(result.interest)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 }
--- investment calculator/src/util/investment.js
+++ investment calculator/src/util/investment.js
@@ -12,16 +12,22 @@ export function calculateInvestmentResults({
 }) {
   const annualData = [];
   let investmentValue = initialInvestment;
+  let data = {};
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
-    annualData.push({
+
+    data = {
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
       valueEndOfYear: investmentValue, // investment value at end of year
       annualInvestment: annualInvestment, // investment added in this year
-    });
+    };
+    data.totalInterest = data.valueEndOfYear - data.annualInvestment * data.year - initialInvestment;
+    data.totalAmountInvested = data.valueEndOfYear - data.totalInterest;
+
+    annualData.push(data);
   }
 
   return annualData;
