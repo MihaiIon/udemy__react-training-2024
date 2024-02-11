65e2a1fba7b9e92a346d1c6fb1ff229556c968e5
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-02-11 11:41:08 -0500

    calculator - fixed html structure with solution structure

--- investment calculator/src/components/InvestmentsForm.jsx
+++ investment calculator/src/components/InvestmentsForm.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 
 export default function CalculatorForm(props) {
   const {
-    formValues: { initialInvestment, annualInvestment, expectedReturn, duration },
+    formValues: {
+      initialInvestment,
+      annualInvestment,
+      expectedReturn,
+      duration,
+    },
     onInitialInvestmentChange,
     onAnnualInvestmentChange,
     onExpectedReturnChange,
@@ -10,51 +15,71 @@ export default function CalculatorForm(props) {
   } = props;
 
   return (
-    <div id="user-input">
-      <label className="input-group" htmlFor="investment-amount">
-        Investment Amount
-        <input
-          type="number"
-          id="investment-amount"
-          name="investment-amount"
-          value={initialInvestment}
-          onChange={event => onInitialInvestmentChange(parseInt(event.target.value))}
-          required
-        />
-      </label>
-      <label className="input-group" htmlFor="annual-investment">
-        Annual Investment
-        <input
-          type="number"
-          id="annual-investment"
-          name="annual-investment"
-          value={annualInvestment}
-          onChange={event => onAnnualInvestmentChange(parseInt(event.target.value))}
-          required
-        />
-      </label>
-      <label className="input-group" htmlFor="expected-return">
-        Expected Return
-        <input
-          type="number"
-          id="expected-return"
-          name="expected-return"
-          value={expectedReturn}
-          onChange={event => onExpectedReturnChange(parseInt(event.target.value))}
-          required
-        />
-      </label>
-      <label className="input-group" htmlFor="investment-duration">
-        Investment Duration
-        <input
-          type="number"
-          id="investment-duration"
-          name="investment-duration"
-          value={duration}
-          onChange={event => onInvestmentDurationChange(parseInt(event.target.value))}
-          required
-        />
-      </label>
-    </div>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="investment-amount">
+            Investment Amount
+            <input
+              type="number"
+              id="investment-amount"
+              name="investment-amount"
+              value={initialInvestment}
+              onChange={(event) =>
+                onInitialInvestmentChange(parseInt(event.target.value))
+              }
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="annual-investment">
+            Annual Investment
+            <input
+              type="number"
+              id="annual-investment"
+              name="annual-investment"
+              value={annualInvestment}
+              onChange={(event) =>
+                onAnnualInvestmentChange(parseInt(event.target.value))
+              }
+              required
+            />
+          </label>
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Return
+            <input
+              type="number"
+              id="expected-return"
+              name="expected-return"
+              value={expectedReturn}
+              onChange={(event) =>
+                onExpectedReturnChange(parseInt(event.target.value))
+              }
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="investment-duration">
+            Investment Duration
+            <input
+              type="number"
+              id="investment-duration"
+              name="investment-duration"
+              value={duration}
+              onChange={(event) =>
+                onInvestmentDurationChange(parseInt(event.target.value))
+              }
+              required
+            />
+          </label>
+        </p>
+      </div>
+    </section>
   );
 }
