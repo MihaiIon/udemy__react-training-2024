9cb86611b7d1d4fde20720a5740d31220e58686e
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-02-18 16:43:15 -0500

    final-count - created timer challenge component

---
+++ final-count/src/components/TimerChallenge.jsx
@@ -0,0 +1,54 @@
+import { useRef, useState } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timerRef = useRef(null);
+
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const setTimer = () => {
+    if (timerRunning) return;
+
+    timerRef.current = setTimeout(() => {
+      setTimerExpired(true);
+      setTimerRunning(false);
+
+      clearTimeout(timerRef.current);
+      timerRef.current = null;
+    }, targetTime * 1000);
+  };
+
+  const startTimer = () => {
+    if (timerRunning) return;
+    setTimer();
+
+    setTimerExpired(false);
+    setTimerRunning(true);
+  };
+
+  const stopTimer = () => {
+    if (!timerRunning) return;
+    setTimerRunning(false);
+
+    clearTimeout(timerRef.current);
+    timerRef.current = null;
+  };
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={!timerRunning ? startTimer : stopTimer}>
+          {!timerRunning ? "Start" : "Stop"}
+        </button>
+      </p>
+      <p className={timerRunning ? "active" : ""}>
+        {timerRunning ? "Time is running" : "Timer is stopped"}
+      </p>
+    </section>
+  );
+}
